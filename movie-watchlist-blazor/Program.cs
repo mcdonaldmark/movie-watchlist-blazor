46bf09f11ec259a7aae5bc35df4a28a4a242b03b
mcdonaldmark <markamcdonald7@gmail.com>
2026-05-16 05:39:33 -0600

    sync

--- movie-watchlist-blazor/Program.cs
+++ movie-watchlist-blazor/Program.cs
@@ -3,7 +3,6 @@ using movie_watchlist_blazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -13,11 +12,9 @@ builder.Services.AddSingleton<AppState>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
