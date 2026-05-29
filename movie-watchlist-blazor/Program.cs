29e69c3d168619e99393dcb06b4195abb4587bea
Blaine <blainenewvillage@gmail.com>
2026-05-29 07:25:45 +0400

    Db Setup 2.0 : Added Movie Model and DB

--- movie-watchlist-blazor/Program.cs
+++ movie-watchlist-blazor/Program.cs
@@ -1,29 +1,45 @@
-using movie_watchlist_blazor.Components;
-using movie_watchlist_blazor.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using movie_watchlist_blazor;
+using movie_watchlist_blazor.Data;
+using movie_watchlist_blazor.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IMovieService, MockMovieService>();
+builder.Services.AddDefaultIdentity<AppUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
 
-builder.Services.AddSingleton<AppState>();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.EnsureCreated();
+}
+
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    app.UseHsts();
+    app.UseExceptionHandler("/Error");
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
-app.UseAntiforgery();
+app.UseStaticFiles();
+app.UseRouting();
 
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
