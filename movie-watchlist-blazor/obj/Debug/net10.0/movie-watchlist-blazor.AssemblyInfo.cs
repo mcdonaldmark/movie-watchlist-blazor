c21e381268a925072d22b83ce9a36fcae5c6e2e2
mcdonaldmark <markamcdonald7@gmail.com>
2026-06-06 02:13:31 -0600

    commit

--- movie-watchlist-blazor/obj/Debug/net10.0/movie-watchlist-blazor.AssemblyInfo.cs
+++ movie-watchlist-blazor/obj/Debug/net10.0/movie-watchlist-blazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("movie-watchlist-blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef42153e55ac91ffd70df3af7be17fb8c966f819")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07701ea0098391d7a234e9caf7ff3833c66ee21")]
 [assembly: System.Reflection.AssemblyProductAttribute("movie-watchlist-blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("movie-watchlist-blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
