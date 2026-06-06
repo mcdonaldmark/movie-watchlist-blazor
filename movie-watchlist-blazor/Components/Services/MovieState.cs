7e36d3ac6d724aec7ebbf815aa9f1ac1a1399a02
mcdonaldmark <markamcdonald7@gmail.com>
2026-06-06 00:24:03 -0600

    Clean project and remove build artifacts

--- movie-watchlist-blazor/Components/Services/MovieState.cs
+++ movie-watchlist-blazor/Components/Services/MovieState.cs
@@ -4,12 +4,8 @@ namespace movie_watchlist_blazor.Services;
 
 public static class MovieState
 {
-    public static List<MovieDto> Movies { get; set; } = new()
-    {
-        new MovieDto { Id = 1, Title = "Inception", Genre = "Sci-Fi", Year = 2010, AverageRating = 4.8 },
-        new MovieDto { Id = 2, Title = "Interstellar", Genre = "Sci-Fi", Year = 2014, AverageRating = 4.7 },
-        new MovieDto { Id = 3, Title = "The Dark Knight", Genre = "Action", Year = 2008, AverageRating = 4.9 }
-    };
+    // TEMPORARY PLACEHOLDERS ONLY (until API exists)
+    public static List<MovieDto> Movies { get; set; } = new();
 
     public static HashSet<int> WatchedMovieIds { get; set; } = new();
 
@@ -23,4 +19,6 @@ public static class MovieState
 
     public static bool IsWatched(int movieId)
         => WatchedMovieIds.Contains(movieId);
+
+    // FUTURE: these will be replaced by API calls
 }
